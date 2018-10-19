327a5e288bc28849089d52c409467e85cc4da756
gct165201b <43592029+gct165201b@users.noreply.github.com>
2018-10-19 06:08:13 +0500

    UPDATING QUERY
    QUERY IS NOW CREATED USING JOIN ON ROLES AND USERS TABLE

--- cms/includes/login.php
+++ cms/includes/login.php
@@ -1,5 +1,5 @@
 <?php include "db.php";
-
+session_start();  // starting a session.
 
 
     if(isset($_POST['login'])) {
@@ -9,7 +9,7 @@
         
         // CHECK FOR VALUES IN DATABASE
         
-        $query = "SELECT * FROM users WHERE username='$username' AND user_password='$user_password'";
+        $query = "SELECT * FROM users JOIN roles ON users.user_role = roles.role_id WHERE username='$username' AND user_password='$user_password'";
         $select_user_records_result = mysqli_query($connection, $query);
         
         if(!$select_user_records_result) {
@@ -19,6 +19,8 @@
             exit();
         } else {
             if($row = mysqli_fetch_assoc($select_user_records_result)) {
+                // GET DATA FROM THE RESULT SET
+                
                 $db_user_id = $row['user_id'];
                 $db_username = $row['username'];
                 $db_user_password = $row['user_password'];
@@ -26,8 +28,33 @@
                 $db_user_lastname = $row['user_lastname'];
                 $db_user_email = $row['user_email'];
                 $db_user_image = $row['user_image'];
-                $db_user_role = $row['user_role'];
+                $db_user_role = $row['role_title'];
                 $db_user_date = $row['user_date'];
+                
+                
+                // DOUBLE CHECKING IF USERNAME AND PASSWORD IS VALID
+                
+                if($username != $db_username || $user_password != $db_user_password) {
+                    header("Location: ../index.php");
+                } else if($username == $db_username && $user_password == $db_user_password) {
+                    
+                    // CREATE SESSION VARIABLES.
+                    // FIRST START A SESSION ON TOP OF THIS PAGE BY USING session_start().
+                    
+                    
+                    $_SESSION['username'] = $db_username;
+                    $_SESSION['user_firstname'] = $db_user_firstname;
+                    $_SESSION['user_lastname'] = $db_user_lastname;
+                    $_SESSION['user_email'] = $db_user_email;
+                    $_SESSION['user_role'] = $db_user_role;
+                    
+                    header("Location: ../admin/");
+                    
+                }
+                
+            } else {
+                header("Location: ../index.php");
+                exit();
             }
         }
         
