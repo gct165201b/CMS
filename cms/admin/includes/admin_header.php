e557eeddf86185cbf856292642497c9ab4a1e77a
gct165201b <43592029+gct165201b@users.noreply.github.com>
2018-10-19 06:09:42 +0500

    Admin Header Updated
    Now the admin header will not any user Enter the admin panel unless its admin

--- cms/admin/includes/admin_header.php
+++ cms/admin/includes/admin_header.php
@@ -2,6 +2,23 @@
 <?php include "../includes/db.php"; ?>
 
 <?php ob_start(); ?>
+<?php session_start(); ?>
+
+
+
+<?php
+
+if(isset($_SESSION['user_role'])) {
+    if($_SESSION['user_role'] !== 'Admin') {
+        header("Location: ../index.php");
+    }
+}
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
