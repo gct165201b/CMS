1605a4006006a055ed20734b018ee31595e2c19d
gct165201b <43592029+gct165201b@users.noreply.github.com>
2018-10-20 19:59:11 +0500

    CK Editor is added

--- cms/admin/includes/add_post.php
+++ cms/admin/includes/add_post.php
@@ -93,7 +93,7 @@ if(isset($_POST['create_post'])) {
        
        <div class="form-group">
            <label for="title">Post Content</label>
-           <textarea class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
+           <textarea class="form-control" name="post_content" id="editor" cols="60" rows="60"></textarea>
        </div>
        
        
