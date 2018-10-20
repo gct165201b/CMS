70b293f972a9fb3269ce991d220c5bda95fb11cd
gct165201b <43592029+gct165201b@users.noreply.github.com>
2018-10-20 05:52:38 +0500

    User drop down Added
    Users drop down is added and only Admin can see it.

--- cms/admin/includes/admin_navigation.php
+++ cms/admin/includes/admin_navigation.php
@@ -19,16 +19,16 @@
                 
                 
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i><?php echo $_SESSION['username']; ?><b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li>
-                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+                            <a href="profile.php"><i class="fa fa-fw fa-user"></i> Profile</a>
                         </li>
                     
                         
                         <li class="divider"></li>
                         <li>
-                            <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                            <a href="../includes/logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
                         </li>
                     </ul>
                 </li>
@@ -67,27 +67,44 @@
                         <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
                     </li>
                     
-                    <li class="active">
+                    <li>
                         <a href="comments.php"><i class="fa fa-fw fa-file"></i> Comments</a>
                     </li>
                     
                     
                     
-                    <li>
+                    <?php
+                    
+                    if($_SESSION['user_role']) {
+                        if($_SESSION['user_role'] == 'Admin') {
+                            
+                            
+                            
+                            ?>
+                            
+                            
+                            <li>
                         <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="demo" class="collapse">
                             <li>
-                                <a href="#">Dropdown Item</a>
+                                <a href="users.php">View All Users</a>
                             </li>
                             <li>
-                                <a href="#">Dropdown Item</a>
+                                <a href="users.php?source=add_user">Add User</a>
                             </li>
                         </ul>
                     </li>
+                            
+                            
+                            <?php
+                        }
+                    }
+                    
+                    ?>
                     
                     
-                    <li class="active">
-                        <a href="blank-page.html"><i class="fa fa-fw fa-dashboard"></i> Profile</a>
+                    <li>
+                        <a href="profile.php"><i class="fa fa-fw fa-dashboard"></i> Profile</a>
                     </li>
                     
                 </ul>
