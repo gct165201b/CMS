67b1c8302acf392f4fe556e9624cb9d87dadc081
gct165201b <43592029+gct165201b@users.noreply.github.com>
2018-10-20 14:48:02 +0500

    Included Google Charts
    Google charts is now displaying graph as needed for this blog.

--- cms/admin/index.php
+++ cms/admin/index.php
@@ -204,6 +204,77 @@
                
                
 <!--               WIDGETS CODE END       -->
+               
+               
+               
+               <div class="row">
+                   
+                   
+<!--                   CHART SCRIPT     -->
+                  
+                  
+                  
+                  
+                  
+                  
+<script type="text/javascript">
+    google.charts.load('current', {'packages':['bar']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+    ['Data', 'Count'],
+        
+        
+        // Bring Dynamic Data From Database.
+        
+        <?php
+        
+        
+        $element_text = ['Active Posts', 'Users', 'Comments' , 'Categories'];
+        $element_counts = [$post_count, $user_count, $comment_count, $cat_count];
+        
+        
+        for($i = 0 ; $i < 4 ; $i++) {
+            echo "['{$element_text[$i]}', {$element_counts[$i]}],";
+        }
+        
+        
+        
+        
+        ?>
+        
+        
+        
+        
+        
+//    ['Posts', 1000],
+    
+    ]);
+
+    var options = {
+        chart: {
+            title: '',
+            subtitle: '',
+        }
+    };
+
+    var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+    chart.draw(data, google.charts.Bar.convertOptions(options));
+    }
+</script>
+                   
+                   
+<!--    CHART DIV      -->
+                   
+    <div id="columnchart_material" style="width: 'auto'; height:500px;"></div>  
+                   
+                   
+                   
+                   
+                   
+               </div>
                 
                 
                 
